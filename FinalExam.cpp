0081365c0a0865e36a64b2870a430bd398e03232
VaheOh <160004309+VaheOh@users.noreply.github.com>
2024-05-27 12:55:50 +0300

    Main Menu complete
    main menu has been completed, now going to start work on the functions

--- FinalExam.cpp
+++ FinalExam.cpp
@@ -10,15 +10,31 @@ using namespace std;
 int main() {
 
 	double matrix[4][3];
+	const int SIZE = 12;
+	int choice = 0;
+	do {
+		cout << "1) Enter data in matrix (range -2.5 to range 2.5)" << endl;
+		cout << "2) Sum of all numbers" << endl;
+		cout << "3) Show data in Matrix" << endl;
+		cout << "4) Exit" << endl;
+		cin >> choice;
+	} while (choice > 4 or choice < 1);
 
+	switch (choice) {
 
-	cout << "1) Enter data in matrix (range -2.5 to range 2.5)" << endl;
-	cout << "2) Sum of all numbers" << endl;
-	cout << "3) Show data in Matrix" << endl;
-	cout << "4) Exit" << endl;
+	case 1:
 
+		break;
+	case 2:
 
+		break;
+	case 3:
 
+		break;
+	case 4:
+
+		break;
+	}
 
 
 	return 0;
