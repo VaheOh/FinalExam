31150c778ed98b560de7ae955d5a10d04e80e069
VaheOh <160004309+VaheOh@users.noreply.github.com>
2024-05-27 12:50:37 +0300

    Commit 1
    This is the start, no progress done yet

---
+++ FinalExam.cpp
@@ -0,0 +1,25 @@
+
+
+
+
+#include <iostream>
+using namespace std;
+
+
+
+int main() {
+
+	double matrix[4][3];
+
+
+	cout << "1) Enter data in matrix (range -2.5 to range 2.5)" << endl;
+	cout << "2) Sum of all numbers" << endl;
+	cout << "3) Show data in Matrix" << endl;
+	cout << "4) Exit" << endl;
+
+
+
+
+
+	return 0;
+}
